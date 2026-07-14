163e4052bc18a37ea17c1497a6e46bb4f2bf3d9a
Yunusr714 <yusufabdul0709@gmail.com>
2026-07-14 10:14:57 +0530

    feat: configure static exports and add GitHub Actions workflow for automated deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Portfolio_new",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
